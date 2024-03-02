4a5e4c993cb67970ee48f75057f8d2200f47213c
daisuke721 <d.magome.07@gmail.com>
2024-03-02 23:28:36 +0900

    feat: headerの検索ボタンの追加 #12

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,26 @@
 import * as React from 'react'
 
-import { ChakraProvider, Box, Button } from '@chakra-ui/react'
+import { ChakraProvider, Box, Button, chakra, Container, Heading, HStack, Flex, Input } from '@chakra-ui/react'
 
 const App: React.FC = () => {
   return (
       <ChakraProvider>
-        <Box px={10} my={10}>Hello</Box>
-        <Button colorScheme="green">Green button</Button>
+        <chakra.header p={3} bg={'green.300'}>
+          <Container>
+            <HStack spacing={10}>
+              <Heading color={'white'}>App Title</Heading>
+              <Flex>
+                <Box pr={4}>
+                  <Input bg={'white'} placeholder='商品を探す' />
+                </Box>
+                <Box>
+                  <Button>検索</Button>
+                </Box>
+              </Flex>
+            </HStack>
+          </Container>
+        </chakra.header>
+        <Box>TOPページ</Box>
       </ChakraProvider>
   );
 }
