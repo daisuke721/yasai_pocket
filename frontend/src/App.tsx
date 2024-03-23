c35c5a99fe0742840836818220a1d49eab73b477
daisuke721 <d.magome.07@gmail.com>
2024-03-23 21:11:21 +0900

    style: themeの背景色を設定 #12

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,10 +4,12 @@ import { ChakraProvider, Button, Heading, Input, InputGroup, InputLeftElement, I
 import { HamburgerIcon, SearchIcon } from '@chakra-ui/icons';
 import { BsCart } from "react-icons/bs";
 
+import theme from './theme/theme';
+
 const App: React.FC = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <Grid as='header' templateColumns='150px repeat(2, 1fr) 80px' gap={10} h='100px' p='25px' bg='orange.300'>
               <GridItem as='a' _hover={{ cursor: 'pointer' }}>
                 <Heading as='h1' textAlign='center' color='white'>App Title</Heading>
@@ -17,7 +19,7 @@ const App: React.FC = () => {
                   <InputLeftElement pointerEvents={'none'}>
                     <SearchIcon color={'gray.300'} />
                   </InputLeftElement>
-                  <Input borderRadius='25px' bg={'white'} placeholder='商品を探す' _placeholder={{ opacity: 1, color: "gray.400" }} />
+                  <Input borderRadius='25px' bg={'white'} outline='none' placeholder='商品を探す' _placeholder={{ opacity: 1, color: "gray.400" }} />
                   <InputRightElement width="4.5rem">
                     <Button color={'gray'} colorScheme='gray' variant='unstyled' size="sm">検索</Button>
                   </InputRightElement>
