36f689d5766307f22514fe4f78c703e218f7d2d9
daisuke721 <d.magome.07@gmail.com>
2024-05-08 16:36:33 +0900

    cartのルーティングの設定 #17

--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom"
 import { Home } from "../components/pages/Home"
 import { ProductDetail } from "../components/pages/ProductDetail"
 import { ProductList } from "../components/pages/ProductList"
+import { CartItem } from "../components/pages/CartItem"
 
 export const Router: React.FC = () => {
   return (
@@ -11,6 +12,7 @@ export const Router: React.FC = () => {
         <Route index element={<Home />} />
         <Route path="product" element={<ProductDetail />} />
         <Route path="productlist" element={<ProductList />} />
+        <Route path="cart" element={<CartItem />} />
       </Route>
     </Routes>
   )
