cc5dec4732c5417058e6da05af1f839f0ca1e29f
daisuke721 <d.magome.07@gmail.com>
2024-05-31 16:37:29 +0900

    feat: Page404の作成 #21

--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -4,6 +4,7 @@ import { Home } from "../components/pages/Home"
 import { ProductDetail } from "../components/pages/ProductDetail"
 import { ProductList } from "../components/pages/ProductList"
 import { CartItem } from "../components/pages/CartItem"
+import { Page404 } from "../components/pages/Page404"
 
 export const Router: React.FC = () => {
   return (
@@ -13,6 +14,7 @@ export const Router: React.FC = () => {
         <Route path="product" element={<ProductDetail />} />
         <Route path="productlist" element={<ProductList />} />
         <Route path="cart" element={<CartItem />} />
+        <Route  path="*" element={<Page404 />} />
       </Route>
     </Routes>
   )
