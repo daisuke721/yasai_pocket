0968b3b9f34290381b487b67ab4aef1716203839
daisuke721 <d.magome.07@gmail.com>
2024-06-06 22:17:11 +0900

    feat: メインビジュアルの作成 #25

--- frontend/src/components/pages/Home.tsx
+++ frontend/src/components/pages/Home.tsx
@@ -2,11 +2,25 @@ import { Link } from "react-router-dom";
 
 import { HeaderTemplate } from "../templates/HeaderTemplate";
 import { FooterTemplate } from "../templates/FooterTemplate";
+import { Box, Button, Image, Text } from "@chakra-ui/react";
 
 export const Home: React.FC = () => {
+
   return(
     <>
       <HeaderTemplate />
+      {/* メインビジュアル */}
+      <Box position="relative" h={{ base: "300px", md: "500px" }} w="100%" overflow="hidden">
+        <Image src="" alt="Main visual" objectFit="cover" w="100%" h="100%" bg="gray.600" />
+        <Box position="absolute" top="50%" left="50%" transform="translate(-50%, -50%)" textAlign="center">
+          <Text fontSize={{ base: "2xl", md: "4xl" }} color="white" fontWeight="bold">メインビジュアル</Text>
+          <Link to="/productlist">
+            <Button mt={4} size="lg" colorScheme="teal" bg="teal.400" _hover={{ bg: 'teal.300' }} fontSize={{ base: "sm", md: "md" }}>
+              もっと見る
+            </Button>
+          </Link>
+        </Box>
+      </Box>
       <div>
         <p>topページ</p>
       </div>
