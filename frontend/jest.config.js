033296ed54f21ed771c1f38989de5817ef65773b
daisuke721 <d.magome.07@gmail.com>
2024-06-14 23:36:54 +0900

    chore: babel対応の為jest.config.jsの編集 #27

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -3,4 +3,7 @@ module.exports = {
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   roots: ['<rootDir>/src/tests'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
+  },
 };
